e1af2a2a80e758a7c423965443103eb4953e8661
mcdit.shermanfu <sherman.fu@cn.mcd.com>
2019-08-20 14:37:59 +0800

    Springboot-Security Test

---
+++ src/main/java/com/gf/SpringbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.gf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//参考：https://mp.weixin.qq.com/s?__biz=MzI0NTYzMjM0Ng==&mid=2247484315&idx=1&sn=122655740e497e57fa7a5c5c93d15ef4
+@SpringBootApplication
+public class SpringbootSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringbootSecurityApplication.class, args );
+    }
+
+}
